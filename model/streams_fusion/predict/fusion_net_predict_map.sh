f2ec8b07e96642950be5b9c5fe213185bd12ea06
zhongju.liu <zhongju.liu@qunar.com>
2017-11-22 16:46:13 +0800

    modify order risk new

--- model/streams_fusion/predict/fusion_net_predict_map.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH=newkeras/newkeras/external_lib/:$LD_LIBRARY_PATH
-
-newkeras/newkeras/external_lib/ld-linux-x86-64.so.2 newkeras/newkeras/bin/python fusion_net_predict_map.py
